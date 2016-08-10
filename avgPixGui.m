35e0fb6684134b45f12c72605ad0b8e7e4e95ad9
2pRigAcq <nielsenlabmbi@gmail.com>
2016-08-10 13:42:25 -0400

    Make gui load without data
    Also deleted data cursor mode from toolbar

--- avgPixGui.m
+++ avgPixGui.m
@@ -39,26 +39,32 @@ function avgPixGui_OpeningFcn(hObject, ~, handles, varargin)
     handles.exptDetail = exptDetail;
     handles.imagingDetail = imagingDetail;
     
-    % get all details of stimul;i and blanks
-    handles.trialDetail = getTrialDetail(Analyzer);
-    
-    % get time windows depending upon how many frames were collected
-    handles.timeWindows = getTimeWindows(handles.imagingDetail);
-    
-    % get tuning for all pixels
-    [handles.pixelTuning,handles.trialResp] = getPixelTuning...
-        (handles.trialDetail,handles.timeWindows,handles.imagingDetail.imageSize);
+    % get all details of stimuli and blanks
+    if ~isempty(handles.analyzer)
+        handles.trialDetail = getTrialDetail(handles.analyzer);
+        handles.dataLoaded = true;
+    else
+        handles.trialDetail = [];
+        handles.trialDetail.isMultipleDomain = 0;
+        handles.trialDetail.domains{1} = '';
+        handles.dataLoaded = false;
+    end
     
     % gui changes
+    handles.clickToMagnifyData = [4,0.08];
+    handles.buttonDownOnAxis = false;
     set(handles.pulldown_param1,'String',handles.trialDetail.domains);
     set(handles.pulldown_param1,'Value',1);
     set(handles.textbox_moduloValue,'String','180','Enable','off');
     if strcmp(handles.trialDetail.domains{1},'ori')
         set(handles.checkbox_circular,'Value',1);
         handles.plotDetail.param1_circular = true;
+    else
+        set(handles.checkbox_circular,'Value',0);
+        handles.plotDetail.param1_circular = false;
     end
     
-    handles.plotDetail.filterPx = 1;
+    handles.plotDetail.filterPx = 3;
     
     handles.plotDetail.param1name = handles.trialDetail.domains{1};
     handles.plotDetail.param1_modulo = false;
@@ -93,56 +99,69 @@ function avgPixGui_OpeningFcn(hObject, ~, handles, varargin)
         handles.plotDetail.param2mode = [];
     end
     
+    % get time windows depending upon how many frames were collected
+    if ~isempty(handles.imagingDetail)
+        handles.timeWindows = getTimeWindows(handles.imagingDetail);
+    else
+        handles.timeWindows = [];
+        handles.imagingDetail.imageSize = [512 796]; 
+        % hack so gui doesn't throw error when loaded without any data
+    end
+    
+    % get tuning for all pixels
+    if ~isempty(handles.timeWindows)
+        [handles.pixelTuning,handles.trialResp] = getPixelTuning...
+            (handles.trialDetail,handles.timeWindows,handles.imagingDetail.imageSize);
+    else
+        handles.pixelTuning = []; handles.trialResp = [];
+    end
+    
     % get mean image
-%     dispImage = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     
     set(handles.axis_image,'xtick',[],'ytick',[]);
     box(handles.axis_image,'on');
     
+    if ~handles.dataLoaded
+        msgbox('No global data was found. Use the ''Open file...'' button to load data.','No data found','warn');
+    end
+    
     % Update handles structure
     guidata(hObject, handles);
 
-% =========================================================================
-% ========================== TOOLBAR CALLBACKS ============================
-% =========================================================================
-
-% --- Outputs from this function are returned to the command line.
-function varargout = avgPixGui_OutputFcn(hObject, eventdata, handles) 
+function varargout = avgPixGui_OutputFcn(~, ~, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-varargout{1} = handles.output;
+varargout{1} = handles.output;    
+    
+% =========================================================================
+% ========================== TOOLBAR CALLBACKS ============================
+% =========================================================================
 
-% --------------------------------------------------------------------
 function uipushtool_open_ClickedCallback(hObject, eventdata, handles)
 % hObject    handle to uipushtool_open (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% --------------------------------------------------------------------
 function uipushtool_save_ClickedCallback(hObject, eventdata, handles)
 % hObject    handle to uipushtool_save (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% --------------------------------------------------------------------
 function uitoggletool_zoomIn_ClickedCallback(hObject, eventdata, handles)
 % hObject    handle to uitoggletool_zoomIn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% --------------------------------------------------------------------
 function uitoggletool_zoomOut_ClickedCallback(hObject, eventdata, handles)
 % hObject    handle to uitoggletool_zoomOut (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% --------------------------------------------------------------------
-function uitoggletool_pixCursor_ClickedCallback(hObject, eventdata, handles)
-% hObject    handle to uitoggletool_pixCursor (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
@@ -167,6 +186,12 @@ function pulldown_param2_Callback(hObject, ~, handles)
     
 function checkbox_circular_Callback(hObject, ~, handles)
     handles.plotDetail.param1_circular = get(hObject,'Value');
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
     
 function checkbox_modulo_Callback(hObject, ~, handles)
@@ -176,11 +201,22 @@ function checkbox_modulo_Callback(hObject, ~, handles)
     else
         set(handles.textbox_moduloValue,'enable','off');
     end
-        
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
 
 function textbox_moduloValue_Callback(hObject, ~, handles)
     handles.plotDetail.param1_moduloVal = str2double(get(hObject,'String'));
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
 
 function radiobutton_mean_Callback(hObject, ~, handles)
@@ -188,12 +224,24 @@ function radiobutton_mean_Callback(hObject, ~, handles)
         handles.plotDetail.param2mode = 'mean';
         set(handles.pulldown_param2Value,'enable','off');
     end
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
     
 function radiobutton_all_Callback(hObject, ~, handles)
     if get(hObject,'Value')
         handles.plotDetail.param2mode = 'all';
         set(handles.pulldown_param2Value,'enable','off');
+    end
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
     end
     guidata(hObject, handles);
     
@@ -205,11 +253,22 @@ function radiobutton_value_Callback(hObject, ~, handles)
     else
         set(handles.pulldown_param2Value,'enable','off');
     end
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
 
 function pulldown_param2Value_Callback(hObject, ~, handles)
     contents = cellstr(get(hObject,'String'));
     handles.plotDetail.param2val = str2double(contents{get(hObject,'Value')});
+    
+    % get mean image
+    if ~isempty(handles.pixelTuning)
+        [dispImage,handles.plotDetail.param1val] = getImage(handles.pixelTuning,handles.trialDetail,handles.plotDetail);
+        plotImage(dispImage,handles.plotDetail,handles.axis_image);
+    end
     guidata(hObject, handles);
     
 % =========================================================================
@@ -256,9 +315,6 @@ function slider_maskSize_Callback(hObject, ~, handles)
     handles.mask.size = normMaskSize * handles.mask.sizeMult;
     guidata(hObject, handles);
 
-function button_adjustContrast_Callback(~, ~, handles)
-    imcontrast(handles.axis_image);
-
 function button_adjustTimeWindows_Callback(hObject, ~, handles)
     
 % =========================================================================
@@ -270,81 +326,115 @@ function button_adjustTimeWindows_Callback(hObject, ~, handles)
 % =========================================================================
 
 function figure1_WindowButtonDownFcn(hObject, ~, handles) 
-    if handles.clickToMagnify
-        f1 = hObject;
-        a1 = handles.axis_image;
-        a2 = copyobj(a1,f1);
+    mouseLoc = get(handles.axis_image,'currentpoint');
+    mouseLoc = fliplr(ceil(mouseLoc(1,1:2)));  
+    if mouseLoc(1) < handles.imagingDetail.imageSize(1) && ...
+       mouseLoc(2) < handles.imagingDetail.imageSize(2) && ...
+       mouseLoc(1) > 0 && ...
+       mouseLoc(2) > 0
+        handles.buttonDownOnAxis = true;
+        if handles.clickToMagnify
+            f1 = hObject;
+            a1 = handles.axis_image;
+            a2 = copyobj(a1,f1);
 
-        set(f1, ...
-          'UserData',[f1,a1,a2], ...
-          'CurrentAxes',a2);
-        set(a2, ...
-          'UserData',[4,0.05], ...  %magnification, frame size
-          'Color',get(a1,'Color'), ...
-          'xtick',[],...
-          'ytick',[],...
-          'Box','on');
-        xlabel(''); ylabel(''); zlabel(''); title('');
-        set(a1, ...
-          'Color',get(a1,'Color')*0.95);
-        set(f1, ...
-          'CurrentAxes',a1);
-        figure1_WindowButtonMotionFcn(hObject,[],handles);
+            set(f1, ...
+              'UserData',[f1,a1,a2], ...
+              'CurrentAxes',a2);
+            set(a2, ...
+              'UserData',handles.clickToMagnifyData, ...  %magnification, frame size
+              'Color',get(a1,'Color'), ...
+              'xtick',[],...
+              'ytick',[],...
+              'Box','on');
+            xlabel(''); ylabel(''); zlabel(''); title('');
+            set(a1, ...
+              'Color',get(a1,'Color')*0.95);
+            set(f1, ...
+              'CurrentAxes',a1);
+            figure1_WindowButtonMotionFcn(hObject,[],handles);
+        elseif handles.pixelmode && ~isempty(handles.trialResp)
+            plotTuning(mouseLoc,handles.trialResp,handles.plotDetail,...
+                handles.trialDetail,handles.imagingDetail,handles.timeWindows,...
+                handles.axis_tc,handles.axis_tuning)
+        end
     end
+    guidata(hObject, handles);
 
 function figure1_WindowButtonMotionFcn(hObject, ~, handles)
-    if handles.clickToMagnify
-        H = get(hObject,'UserData');
-        if ~isempty(H)
-            f1 = H(1); a1 = H(2); a2 = H(3);
-            a2_param = get(a2,'UserData');
-            f_pos = get(f1,'Position');
-            a1_pos = get(a1,'Position');
+    mouseLoc = get(handles.axis_image,'currentpoint');
+    mouseLoc = fliplr(ceil(mouseLoc(1,1:2)));  
+    if mouseLoc(1) < handles.imagingDetail.imageSize(1) && ...
+       mouseLoc(2) < handles.imagingDetail.imageSize(2) && ...
+       mouseLoc(1) > 0 && ...
+       mouseLoc(2) > 0
+        if handles.clickToMagnify
+            H = get(hObject,'UserData');
+            if ~isempty(H)
+                f1 = H(1); a1 = H(2); a2 = H(3);
+                a2_param = get(a2,'UserData');
+                f_pos = get(f1,'Position');
+                a1_pos = get(a1,'Position');
 
-            [f_cp, a1_cp] = pointer2d(f1,a1);
+                [f_cp, a1_cp] = pointer2d(f1,a1);
 
-            set(a2,'Position',[(f_cp./f_pos(3:4)) 0 0]+a2_param(2)*a1_pos(3)*[-1 -1 2 2]);
-            a2_pos = get(a2,'Position');
+                set(a2,'Position',[(f_cp./f_pos(3:4)) 0 0]+a2_param(2)*a1_pos(3)*[-1 -1 2 2]);
+                a2_pos = get(a2,'Position');
 
-            set(a2,'XLim',a1_cp(1)+(1/a2_param(1))*(a2_pos(3)/a1_pos(3))*diff(get(a1,'XLim'))*[-0.5 0.5]);
-            set(a2,'YLim',a1_cp(2)+(1/a2_param(1))*(a2_pos(4)/a1_pos(4))*diff(get(a1,'YLim'))*[-0.5 0.5]);
+                set(a2,'XLim',a1_cp(1)+(1/a2_param(1))*(a2_pos(3)/a1_pos(3))*diff(get(a1,'XLim'))*[-0.5 0.5]);
+                set(a2,'YLim',a1_cp(2)+(1/a2_param(1))*(a2_pos(4)/a1_pos(4))*diff(get(a1,'YLim'))*[-0.5 0.5]);
+            end
+        elseif handles.pixelmode && handles.buttonDownOnAxis && ~isempty(handles.trialResp)
+            plotTuning(mouseLoc,handles.trialResp,handles.plotDetail,...
+                handles.trialDetail,handles.imagingDetail,handles.timeWindows,...
+                handles.axis_tc,handles.axis_tuning)
         end
     end
+    guidata(hObject, handles);
 
 function figure1_WindowButtonUpFcn(hObject, ~, handles) 
-    if handles.clickToMagnify
-        H = get(hObject,'UserData');
-        f1 = H(1); a1 = H(2); a2 = H(3);
-        set(a1, ...
-          'Color',get(a2,'Color'));
-        set(f1, ...
-          'UserData',[], ...
-          'Pointer','arrow', ...
-          'CurrentAxes',a1);
-        if ~strcmp(get(f1,'SelectionType'),'alt'),
-          delete(a2);
+    mouseLoc = get(handles.axis_image,'currentpoint');
+    mouseLoc = fliplr(ceil(mouseLoc(1,1:2)));  
+    if mouseLoc(1) < handles.imagingDetail.imageSize(1) && ...
+       mouseLoc(2) < handles.imagingDetail.imageSize(2) && ...
+       mouseLoc(1) > 0 && ...
+       mouseLoc(2) > 0
+        handles.buttonDownOnAxis = false;
+        if handles.clickToMagnify
+            H = get(hObject,'UserData');
+            f1 = H(1); a1 = H(2); a2 = H(3);
+            set(a1, ...
+              'Color',get(a2,'Color'));
+            set(f1, ...
+              'UserData',[], ...
+              'Pointer','arrow', ...
+              'CurrentAxes',a1);
+            if ~strcmp(get(f1,'SelectionType'),'alt'),
+              delete(a2);
+            end
         end
     end
+    guidata(hObject, handles);
 
 function figure1_KeyPressFcn(hObject, ~, handles)
     if handles.clickToMagnify
         H = get(hObject,'UserData');
         if ~isempty(H)
             f1 = H(1); a1 = H(2); a2 = H(3);
-            a2_param = get(a2,'UserData');
             if (strcmp(get(f1,'CurrentCharacter'),'+') || strcmp(get(f1,'CurrentCharacter'),'='))
-             a2_param(1) = a2_param(1)*1.2;
+                handles.clickToMagnifyData(1) = handles.clickToMagnifyData(1)*1.2;
             elseif (strcmp(get(f1,'CurrentCharacter'),'-') || strcmp(get(f1,'CurrentCharacter'),'_'))
-             a2_param(1) = a2_param(1)/1.2;
+                handles.clickToMagnifyData(1) = handles.clickToMagnifyData(1)/1.2;
             elseif (strcmp(get(f1,'CurrentCharacter'),'<') || strcmp(get(f1,'CurrentCharacter'),','))
-             a2_param(2) = a2_param(2)/1.2;
+                handles.clickToMagnifyData(2) = handles.clickToMagnifyData(2)/1.2;
             elseif (strcmp(get(f1,'CurrentCharacter'),'>') || strcmp(get(f1,'CurrentCharacter'),'.'))
-             a2_param(2) = a2_param(2)*1.2;
+                handles.clickToMagnifyData(2) = handles.clickToMagnifyData(2)*1.2;
             end;
-            set(a2,'UserData',a2_param);
+            set(a2,'UserData',handles.clickToMagnifyData);
             figure1_WindowButtonMotionFcn(hObject,[],handles);
         end
     end
+    guidata(hObject, handles);
 
 function [fig_pointer_pos, axes_pointer_val] = pointer2d(fig_hndl,axes_hndl)
     if (nargin == 0), fig_hndl = gcf; axes_hndl = gca; end;
@@ -368,6 +458,15 @@ function [fig_pointer_pos, axes_pointer_val] = pointer2d(fig_hndl,axes_hndl)
 % =========================================================================
 % ================== CLICK TO MAGNIFY DONE ================================
 % =========================================================================
+
+% =========================================================================
+% ========================== PIXEL CLICKED ================================
+% =========================================================================
+    
+% =========================================================================
+% ========================== PIXEL CLICKED DONE ===========================
+% =========================================================================
+
 
 % =========================================================================
 % ============================== CREATEFNs ================================
