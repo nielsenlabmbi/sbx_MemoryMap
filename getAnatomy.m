47359810ef3213542d10a15a290176687f22417d
2pRigAcq <nielsenlabmbi@gmail.com>
2016-08-12 15:11:23 -0400

    Bug: Skip n trials skips no trials when 1

--- getAnatomy.m
+++ getAnatomy.m
@@ -1,13 +1,13 @@
 function anatomy = getAnatomy(skipNtrials)
     global pixelTc;
     
-    if ~exist('skipNtrials','var'); skipNtrials = 5; end
+    if ~exist('skipNtrials','var'); skipNtrials = 2; end
     
     anatomy = zeros(size(pixelTc{1},1),size(pixelTc{1},2),length(1:skipNtrials:length(pixelTc)));
-    for ii=1:skipNtrials:length(pixelTc)
+    for ii=1:(skipNtrials-1):length(pixelTc)
         anatomy(:,:,ii) = mean(pixelTc{ii},3);
     end
     anatomy = mean(anatomy,3);
-    anatomy = imadjust(anatomy);
+%     anatomy = imadjust(anatomy);
 end
 
