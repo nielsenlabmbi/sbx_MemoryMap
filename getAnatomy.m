7177db14ba91d80394931a0a69cf7aab34cbf730
2pRigAcq <nielsenlabmbi@gmail.com>
2016-10-12 15:19:57 -0400

    Vectorizing getAnatomy

--- getAnatomy.m
+++ getAnatomy.m
@@ -1,13 +1,8 @@
-function anatomy = getAnatomy(skipNtrials)
+function anatomy = getAnatomy()
     global pixelTc;
     
-    if ~exist('skipNtrials','var'); skipNtrials = 2; end
-    
-    anatomy = zeros(size(pixelTc{1},1),size(pixelTc{1},2),length(1:skipNtrials:length(pixelTc)));
-    for ii=1:(skipNtrials-1):length(pixelTc)
-        anatomy(:,:,ii) = mean(pixelTc{ii},3);
-    end
-    anatomy = mean(anatomy,3);
-%     anatomy = imadjust(anatomy);
+    a = cellfun(@(x)mean(x,3),pixelTc,'uniformoutput',false);
+    a = cell2mat(reshape(a,[1 1 27]));
+    anatomy = std(a,[],3);
 end
 
